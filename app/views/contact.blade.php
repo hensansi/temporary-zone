be9c5ae4e945852c2713eaf7c9cbd9cd7c2fe533
hensansi <h9nrik@gmail.com>
2014-09-18 18:44:02 +0100

    Updated email on contact page

--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="text-box">
-        <p> I am working on this page meanwhile you can contact me directly to {{  HTML::mailto('hensansi@gmail.com');}}. Have a nice day.
+        <p> I am working on this page meanwhile you can contact me directly to {{  HTML::mailto('me@henriquesilva.pt');}}. Have a nice day.
         </p>
     </div>
 @stop
