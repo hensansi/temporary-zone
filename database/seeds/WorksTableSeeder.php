9eee56c43d14049bfdc9edd83326e12b9c1064fd
hensansi <git@hensansi.me>
2024-08-22 20:35:39 +0200

    feat: update links for the uni projects
    Will try to restore them one by one in the following days

--- database/seeds/WorksTableSeeder.php
+++ database/seeds/WorksTableSeeder.php
@@ -15,7 +15,7 @@ class WorksTableSeeder extends Seeder {
         Work::create([
             'name' => 'Glomeradados - Databases',
             'client' => 'Databases - University',
-            'url' => 'bd.henriquesilva.pt',
+            'url' => 'bd.hensansi.me',
             'id_category' => 1,
             'a' => 'glomeradados',
             'descricao' => 'The aim of this project is to provide students with experience in developing database systems. The project is based in industry’s best practices for software development, thus students will experience all the main stages of a common software project, from the very beginning to production release.'
@@ -24,7 +24,7 @@ class WorksTableSeeder extends Seeder {
         Work::create([
             'name' => 'Data Visualization in Social Network',
             'client' => 'Technology for Social Web - University',
-            'url' => 'taws.henriquesilva.pt',
+            'url' => 'taws.hensansi.me',
             'id_category' => 1,
             'a' => 'data_visualization',
             'descricao' => 'The purpose of the website was to get to know more about methodologies and techonologies used in the social web context, by creating a website that used the Meetups API.'
@@ -33,7 +33,7 @@ class WorksTableSeeder extends Seeder {
         Work::create([
             'name' => 'Releituras -  Archive of Concrete Poetry',
             'client' => 'Design IV - University',
-            'url' => 'd4.henriquesilva.pt',
+            'url' => 'd4.hensansi.me',
             'id_category' => 1,
             'a' => 'releituras',
             'descricao' => 'This website acted as an archive of performances done during the classes. The program had its base in concrete poetry and it had support weekly shows from various writers, artists and performers.'
@@ -44,7 +44,7 @@ class WorksTableSeeder extends Seeder {
         Work::create([
             'name' => 'EXP - TV Channel Identity',
             'client' => 'Digital Prodution - University',
-            'url' => 'pd.henriquesilva.pt',
+            'url' => 'pd.hensansi.me',
             'id_category' => 1,
             'a' => 'exp',
             'descricao' => 'This website acted as support for an identity channel project, to host and show all the content produced during the classes.'
@@ -54,30 +54,32 @@ class WorksTableSeeder extends Seeder {
         Work::create([
             'name' => 'Free Seasonal Publication - DEI/CISUC',
             'client' => 'Design III - University',
-            'url' => 'd3.henriquesilva.pt',
+            'url' => 'd3.hensansi.me',
             'id_category' => 1,
             'a' => 'newsletter',
             'descricao' => 'The project was about transforming a paper newsletter into a virtual one. While trying to maintain its identity and not emulating but transforming the way it could be seen and accessed.'
+        ]);
+
+        Work::create([
+            'name' => 'Queima 24! - Short Film Contest',
+            'client' => 'tvAAC',
+            'url' => 'queima24.hensansi.me',
+            'id_category' => 1,
+            'a' => 'queima24',
+            'descricao' => 'Built in 2010, this was the initiation into the web development world. With the exception of the javascript navigation, the typo photography and design were made by me. The website was recovered from WebArchive as tv.aac.uc.pt/queima24'
         ]);
     /*
 
         Work::create([
             'name' => 'Museu da Pedra',
             'client' => 'Internet Technology - University',
-            'url' => 'ti.henriquesilva.pt',
+            'url' => 'ti.hensansi.me',
             'id_category' => 1,
             'a' => 'museu',
             'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum quam eu commodo sollicitudin. Duis sed elementum felis, ornare gravida justo. Etiam vel nisi condimentum, lacinia tortor a, egestas lacus. Suspendisse interdum enim vitae odio efficitur molestie. Proin nec molestie nisl.'
         ]);
 
-        Work::create([
-            'name' => 'Queima 24! - Short Film Contest',
-            'client' => 'tvAAC',
-            'url' => 'tv.aac.uc.pt/queima24/',
-            'id_category' => 1,
-            'a' => 'queima24',
-            'descricao' => 'Built in 2010, this was the initiation into the web development world. With the exception of the javascript navigation, the typo photography and design were made by me.'
-        ]);
+
 
     */
     }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
     <div class="box">
         <p>
             I am working on this page meanwhile you can contact me directly to 
-            <a href="mailto:me@henriquesilva.pt">me@henriquesilva.pt</a>. 
+            <a href="mailto:me@hensansi.me">tz2024@hensansi.me</a>. 
             Have a nice day.
         </p>
     </div>
